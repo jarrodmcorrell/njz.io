8635b9f1ecb2704f992d157279d754a8b9f2ec3b
jarrodmcorrell <67766756+jarrodmcorrell@users.noreply.github.com>
2020-09-16 17:13:21 -0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 "# njz.io" 
+
+Front-End
+- React
+- Material UI/CSS
+
+Back-End
+- Firebase Database
+- Firebase Authentication
---
+++ njzapp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>HELLO</h1>
+      {/*Header*/}
+      {/*Images*/}
+    </div>
+  );
+}
+
+export default App;
